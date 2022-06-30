96454d3c3b6df0ad7cd448d79027a1dc8f93c9b9
Farina Alexey <farik88@ua.fm>
2022-06-30 18:18:52 +0300

    Beer card template work

--- src/components/BeerListItem.tsx
+++ src/components/BeerListItem.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import cl from "../styles/components/BeerListItem.module.scss"
 import {IBeer} from "../types/IBeer";
 import {Card} from "@mui/material";
+import ClampLines from 'react-clamp-lines';
 
 interface BeerListItemProps {
   beer: IBeer
@@ -14,7 +15,7 @@ const BeerListItem = ({beer}: BeerListItemProps) => {
         <img src={beer.image_url !== null ? beer.image_url : '/def_bottle.png'} alt={beer.name + '_image'} />
       </div>
       <div className={cl.item__infoSection}>
-        <h2 className={cl.item__beerName}>{beer.name}</h2>
+        <h2 className={cl.item__title}>{beer.name}</h2>
         <div className={cl.item__taglineBlock}>
           <span className={cl.item__taglineText}>
             {beer.tagline}
@@ -26,12 +27,32 @@ const BeerListItem = ({beer}: BeerListItemProps) => {
           </span>
         </div>
       </div>
-      <div>
-        {beer.description}
+      <div className={cl.item__descSection}>
+        <ClampLines
+          text={beer.description}
+          id={"clamp_beer_desc_" + beer.id}
+          lines={1.9}
+          ellipsis="..."
+          moreText="More"
+          lessText="Less"
+          className={cl.clampContent}
+          innerElement="p"
+        />
       </div>
-      <hr/>
-      <div>
-        {beer.food_pairing.join('. ')}
+      <div className={cl.item__foodSection}>
+        <h3 className={cl.item__subTitle}>
+          Taste with:
+        </h3>
+        <ClampLines
+          text={beer.food_pairing.join('. ')}
+          id={"clamp_beer_food_" + beer.id}
+          lines={1}
+          ellipsis="..."
+          moreText="More"
+          lessText="Less"
+          className={cl.clampContent}
+          innerElement="p"
+        />
       </div>
     </Card>
   );
